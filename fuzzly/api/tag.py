66f441bb28bf43cb51f2e6333ba281e3ae1a864e
dani <29378233+DanielleMiu@users.noreply.github.com>
2023-05-05 01:09:55 -0400

    fix internal tags endpoint, update tags endpoint

--- fuzzly/api/tag.py
+++ fuzzly/api/tag.py
@@ -8,4 +8,4 @@ from ..models.tag import Tag, TagGroups
 FetchTag: Gateway = Gateway(TagHost + '/v1/tag/{tag}', Tag, method='GET')
 
 # Usage: FetchPostTags(post_id='abcd1234')
-FetchPostTags: Gateway = Gateway(TagHost + '/v1/post/{post_id}', TagGroups, method='GET')
+FetchPostTags: Gateway = Gateway(TagHost + '/v1/tags/{post_id}', TagGroups, method='GET')
